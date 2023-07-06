29c2648d488f460ca35c1bf7c7b1d96dfb530e2b
wen <nykk45@gmail.com>
2023-07-06 15:31:57 +0800

    新增 票選活動倒數計時、文章目錄tab切換位置錯位問題

--- static/js/article.js
+++ static/js/article.js
@@ -2,13 +2,24 @@
 const now_today = dayjs().format("YYYY-MM-DD");
 $("#article_date").html(now_today);
 
+$("#content_tabs > li > a").click(function () {
+    var target = $(this).attr("href");
+    $("html, body").animate(
+        {
+            scrollTop: $(target).offset().top - 100,
+        },
+        300
+    );
+    return false;
+});
+
 function like(obj) {
     const obj_node = obj.parentNode; //tr
-    const has_like = $(obj_node).parent().find("i.fa-heart").hasClass("ct-txt-1");
+    const has_like = $(obj_node).parent().find("i.fa-heart").hasClass("ct-txt-2");
     let like_count = parseInt($(obj_node).parent().find("span").html());
 
     if (has_like) {
-        $(obj_node).parent().find("i.fa-heart").removeClass("ct-txt-1");
+        $(obj_node).parent().find("i.fa-heart").removeClass("ct-txt-2");
         $(obj_node).parent().find("i.fa-heart").addClass("ct-sub-1");
         $(obj_node)
             .parent()
@@ -16,7 +27,7 @@ function like(obj) {
             .html(like_count - 1);
     } else {
         $(obj_node).parent().find("i.fa-heart").removeClass("ct-sub-1");
-        $(obj_node).parent().find("i.fa-heart").addClass("ct-txt-1");
+        $(obj_node).parent().find("i.fa-heart").addClass("ct-txt-2");
         $(obj_node)
             .parent()
             .find("span.like_count")
@@ -29,9 +40,18 @@ function collect(obj) {
     const no_collect = $(obj_node).parent().find("i.fa-bookmark").hasClass("far");
     if (no_collect) {
         $(obj_node).parent().find("i.fa-bookmark").removeClass("far ct-sub-1");
-        $(obj_node).parent().find("i.fa-bookmark").addClass("fas ct-txt-1");
+        $(obj_node).parent().find("i.fa-bookmark").addClass("fas ct-txt-2");
     } else {
-        $(obj_node).parent().find("i.fa-bookmark").removeClass("fas ct-txt-1");
+        $(obj_node).parent().find("i.fa-bookmark").removeClass("fas ct-txt-2");
         $(obj_node).parent().find("i.fa-bookmark").addClass("far ct-sub-1");
     }
 }
+
+//療心室-票選活動 倒數計時
+$(".countdown")._countTime("2023-08-03 12:30:00", {
+    isActive: true,
+    str: {
+        title: "活動截止：",
+        day: "日",
+    },
+});
